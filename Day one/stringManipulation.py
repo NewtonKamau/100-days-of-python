5d3a61976d58d4892808ab172ef24bddb5e62b89
NewtonKamau <kamaunewton78@gmail.com>
2024-11-07 11:32:33 +0300

    learning string manipulation and created a band generator program.

--- Day one/stringManipulation.py
+++ Day one/stringManipulation.py
@@ -1,1 +1,15 @@
-print("this is day one \n let me see if I'm upto the challenge\n hopefully I become an AI engineer after this" + " "+"Newton God got you")
+#band name generator program
+#1. create a greeting for your program/
+
+#2. Ask the user the city they grew up in
+
+#3. Ask the user for the name of a pet
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in? \n")
+pet = input("What's name of the your pet ? \n")
+
+print("Your band name is \n" + city+pet)
+ 
