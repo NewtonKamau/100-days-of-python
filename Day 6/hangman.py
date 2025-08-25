d9c8e2273f008112d54e2a1508f5e442b1d23fd3
NewtonKamau <kamaunewton78@gmail.com>
2025-08-25 13:20:51 +0300

    Refactor hangman game logic and add stages for visual representation

--- Day 6/hangman.py
+++ Day 6/hangman.py
@@ -1,39 +1,44 @@
 import random
+from stages import stages
 
-# step one
-word_list = ["python", "java", "javascript", "csharp"]
-
-# TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word
+# Word bank
+word_list = ["python", "java", "javascript", "csharp", "hangman", "developer"]
 chosen_word = random.choice(word_list)
-
-# TODO-2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-
-# TODO-4 create an empty list called display
-# for each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was 'apple', display should be ['_', '_', '_', '_', '_']
-display = []
 word_length = len(chosen_word)
-for _ in range(word_length):
-    display += "_"
 
-# TODO-5 loop through each position in the chosen_word
-# if the letter at that position matches the guess, update the display list.
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+# Setup
+display = ["_"] * word_length
+lives = len(stages) - 1
+guessed_letters = set()
 
-# TODO-6 print 'display' and you should see the guessed letter in the correct position
-print(display)
+print("Welcome to Hangman!")
 
-# use a while loop to let the user guess again until word is fully revealed
-while "_" in display:
+# Main game loop
+while "_" in display and lives > 0:
+    print("\n" + " ".join(display))
     guess = input("Guess a letter: ").lower()
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-    print(display)
 
-print("You've won!")
+    # Validate repeated guesses
+    if guess in guessed_letters:
+        print(f"You already guessed '{guess}'. Try another letter.")
+        continue
+    guessed_letters.add(guess)
+
+    # Check guess
+    if guess in chosen_word:
+        for position in range(word_length):
+            if chosen_word[position] == guess:
+                display[position] = guess
+        print("Good guess!")
+    else:
+        lives -= 1
+        print(f"Wrong guess! You lose a life. Lives left: {lives}")
+        print(stages[lives])
+
+# Outcome
+if "_" not in display:
+    print("\n" + " ".join(display))
+    print("🎉 You've won! The word was:", chosen_word)
+else:
+    print(stages[0])
+    print("💀 You lost! The word was:", chosen_word)
